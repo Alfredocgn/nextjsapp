d47e4927156778c8b9e3db55b89348f2535001b1
Alfredocgn <alfredocgn@gmail.com>
2024-01-06 11:33:49 -0300

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,28 @@ import Image from "next/image";
 
 const AboutPage = () => {
   return (
-    <div>
-      <div className="relative w-full h-[500px] ">
-        <Image src="/about.png" alt="about image" fill/>
+    <div className="flex gap-24">
+      <div className="flex-1 flex flex-col gap-12 ">
+        <h2 className="text-[#3673fd]">About Agency</h2>
+        <h1 className=" text-7xl ">We create digital ideas that are bigger, bolder, braver and better.</h1>
+        <p className="text-lg font-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum possimus aspernatur nisi totam earum aut nam voluptate dolor numquam modi, explicabo neque, animi iste aliquam officia accusantium? Sed, in temporibus.</p>
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-2">
+            <h3 className="text-[#3673fd] text-3xl">10 K+</h3>
+            <p>Year of experience</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h3 className="text-[#3673fd] text-3xl">10 K+</h3>
+            <p>Year of experience</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h3 className="text-[#3673fd] text-3xl">10 K+</h3>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 relative">
+        <Image className=" object-contain " src="/about.png" alt="about image" fill />
       </div>
     </div>
   )
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return(
     <div className="flex justify-between items-center text-gray-700 h-24 ">
       <div className=" font-bold  ">lamadev</div>
-      <div className=" text-lg ">Lama creatiuve thoughts agency All rights reserved.</div>
+      <div className=" text-lg ">Lama creative thoughts agency All rights reserved.</div>
     </div>
   )
 }
